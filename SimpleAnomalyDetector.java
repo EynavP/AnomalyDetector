fc22c2cbc327168901fa496e92ff9febc65aa727
Eynav Ptia <73962261+EynavP@users.noreply.github.com>
2021-01-26 10:26:29 +0200

    Add files via upload
    Milestone 2

---
+++ SimpleAnomalyDetector.java
@@ -0,0 +1,112 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SimpleAnomalyDetector implements TimeSeriesAnomalyDetector
+{
+	ArrayList<CorrelatedFeatures> correlatedArray = new ArrayList<CorrelatedFeatures>();
+
+	public float[] changeTofloat(ArrayList<Float> a)
+	{
+		float[] fArray = new float[a.size()];
+		for (int i=0;i<a.size();i++) 
+		{
+		    fArray[i] = a.get(i);
+		}
+		return fArray;
+	}
+	public Point[] changeToPoint(float[]a,float[]b)
+	{
+		Point[] p = new Point[a.length];
+		for(int i=0;i<a.length;i++)
+		{
+			p[i]=new Point(a[i],b[i]);
+		}
+		return p;
+	}
+	public boolean alreadyCor(String a,String b)
+	{
+		for(int i=0;i<this.correlatedArray.size();i++)
+		{
+			if(this.correlatedArray.get(i).feature1.equals(b)&&(this.correlatedArray.get(i).feature2.equals(a)))
+				return true;
+		}
+		return false;
+	}
+	
+	public void learnNormal(TimeSeries ts) 
+	{
+		int size= ts.getColNum();
+		int place=0;
+		float max=0;
+		for(int i=0;i<size-1;i++)
+		{
+			float[] property1 = changeTofloat(ts.table.get(i).numbers);
+			ts.table.get(i).setCoraleted(ts.table.get(i+1).propertyName);
+			place = 0;
+			for(int j=0;j<size;j++)
+			{				
+				if(i!=j)
+				{
+					float[] property2 = changeTofloat(ts.table.get(j).numbers);
+					//max =StatLib.pearson(property1,property2);
+					float maybeMax = Math.abs(StatLib.pearson(changeTofloat(ts.table.get(i).numbers), changeTofloat(ts.table.get(j).numbers)));
+					if(max<=maybeMax&&maybeMax>ts.coralation)
+					{
+						max=maybeMax;
+						ts.table.get(i).setCoraleted(ts.table.get(j).propertyName);
+						place=j;
+					}
+				}
+			}
+			if(max!=0)
+			{
+				Point[] temp = changeToPoint(property1,changeTofloat(ts.table.get(place).numbers));
+				Line tempLine = StatLib.linear_reg(temp);
+				float th = StatLib.dev(temp[0],tempLine);
+				for(int k=0;k<temp.length;k++)
+				{
+					float help = StatLib.dev(temp[k],tempLine);
+					if(th<help)
+						th=help;
+				}
+				if(alreadyCor(ts.table.get(i).propertyName,ts.table.get(i).coraleted)==false)
+				{
+					th=(float) (th+0.027);
+					CorrelatedFeatures helpCor = new CorrelatedFeatures(ts.table.get(i).propertyName, ts.table.get(i).coraleted,max,tempLine,th);
+					correlatedArray.add(helpCor);
+				}
+			}
+			max=0;
+		}
+	}
+	public List<AnomalyReport> detect(TimeSeries ts)
+	{
+		ArrayList<AnomalyReport> resultList = new ArrayList<AnomalyReport>();
+		for(int i=0;i<this.correlatedArray.size();i++)
+		{
+			ArrayList<Float>property1 = ts.getDataByName(this.correlatedArray.get(i).feature1);
+			ArrayList<Float>property2 = ts.getDataByName(this.correlatedArray.get(i).feature2);
+			Point[] arrayPoint = changeToPoint(changeTofloat(property1),changeTofloat(property2));
+			String f1 = this.correlatedArray.get(i).feature1;
+			String f2 = this.correlatedArray.get(i).feature2;
+			String desc = f1+"-"+f2;
+			for(int j=0;j<arrayPoint.length;j++)
+			{
+				float tempValue = StatLib.dev(arrayPoint[j],this.correlatedArray.get(i).lin_reg);
+				if(tempValue>this.correlatedArray.get(i).threshold)
+				{
+					AnomalyReport report = new AnomalyReport(desc, j+1);	
+					resultList.add(report);
+				}					
+			}
+		}
+		return resultList;
+	}
+	
+	public List<CorrelatedFeatures> getNormalModel(){
+		return this.correlatedArray;
+	}
+}
---
+++ TimeSeries.java
@@ -0,0 +1,122 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class TimeSeries 
+{
+	//Data class for column details
+	public class Data{
+		
+		String propertyName;
+		ArrayList<Float> numbers;
+		String coraleted;
+		//Constructor
+		public Data(String propertyName) 
+		{
+			this.setPropertyName(propertyName);
+			this.setCoraleted(null);
+			this.numbers = new ArrayList<Float>();
+		}
+		//Getters and Setters
+		public String getPropertyName() {
+			return propertyName;
+		}
+		public void setPropertyName(String propertyName) {
+			this.propertyName = propertyName;
+		}
+		public String getCoraleted() {
+			return coraleted;
+		}
+		public void setCoraleted(String coraleted) {
+			this.coraleted = coraleted;
+		}
+	}
+	
+	//TimeSerie completion continue
+	ArrayList<Data> table = new ArrayList<>();
+	String fileName;
+	int colNum=0;
+	int lineNum=0;
+	double coralation = 0.9;
+	
+	
+	public double getCoralation() {
+		return coralation;
+	}
+
+
+	public void setCoralation(double coralation) {
+		this.coralation = coralation;
+	}
+
+
+	public TimeSeries(String csvFileName)
+	{
+		this.fileName=csvFileName;
+		Path pathToFile = Paths.get(csvFileName);
+		try {
+			BufferedReader br = Files.newBufferedReader(pathToFile,
+			        StandardCharsets.US_ASCII);
+			String rightLine = br.readLine();
+			String[] firstLine = rightLine.split(",");
+			for(int i=0;i<firstLine.length;i++)
+			{
+				Data temp = new Data(firstLine[i]);
+				this.table.add(temp);
+				this.colNum++;
+			}
+	    rightLine = br.readLine();
+	    while(rightLine!=null)
+	    {
+	    	firstLine = rightLine.split(",");
+	    	for(int i=0;i<this.getColNum();i++)
+	    	{
+	    		float f = Float.parseFloat(firstLine[i]);
+	    		this.table.get(i).numbers.add(f);
+	    	}
+	    	rightLine = br.readLine();
+	    }
+	    br.close();
+	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//Getters and Setters
+
+	public int getColNum() {
+		return colNum;
+	}
+
+	public void setColNum(int colNum) {
+		this.colNum = colNum;
+	}
+
+	public int getLineNum() {
+		return lineNum;
+	}
+
+	public void setLineNum(int lineNum) {
+		this.lineNum = lineNum;
+	}
+	
+	public ArrayList<Float> getDataByName(String name )
+	{
+		for(int i=0;i<this.colNum;i++)
+		{
+			if(this.table.get(i).propertyName.equals(name))
+				return this.table.get(i).numbers;
+		}
+		return null;
+	}
+	
+}
